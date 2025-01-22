6cadabbaed606b8f70a07ede381bbc1c9a79a493
liamcharger <chargerelectronics19@gmail.com>
2025-01-22 15:54:20 -0500

    Initial Commit

---
+++ WakatimeWidget/WakatimeWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  WakatimeWidgetApp.swift
+//  WakatimeWidget
+//
+//  Created by Liam Willey on 1/22/25.
+//
+
+import SwiftUI
+
+@main
+struct WakatimeWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
